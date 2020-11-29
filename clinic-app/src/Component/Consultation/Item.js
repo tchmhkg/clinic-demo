1a4ab88a13b937bcb6600d9742eb522d5b9e52e0
Truman Cheung <>
2020-11-30 00:18:23 +0800

    add dollar formatter

--- clinic-app/src/Component/Consultation/Item.js
+++ clinic-app/src/Component/Consultation/Item.js
@@ -3,11 +3,16 @@ import { View, Text } from "react-native";
 import Styled from "styled-components/native";
 import { useNavigation } from "@react-navigation/native";
 
+import { dollarFormat } from '~/Helper';
 const ListItem = Styled.TouchableOpacity`
   padding: 10px;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+`;
+
+const RightCol = Styled.View`
+  align-items: flex-end;
 `;
 
 const Item = ({ item }) => {
@@ -26,9 +31,10 @@ const Item = ({ item }) => {
         <Text>Doctor Name: {item?.doctorName}</Text>
         <Text>Patient Name: {item?.patientName}</Text>
       </View>
-      <View>
-        <Text>${item?.consultationFee}</Text>
-      </View>
+      <RightCol>
+        <Text>{item?.date}</Text>
+        <Text>${dollarFormat(item?.consultationFee)}</Text>
+      </RightCol>
     </ListItem>
   );
 };
